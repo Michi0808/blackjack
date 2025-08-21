4693fd411c8c95511f8258733e6ebf58ae2cf593
jagar08 <jagar008@gmail.com>
2025-08-21 15:19:09 +0200

    Fix Score calculation

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const suit = ["c", "d", "h", "s"];
 const cardNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 const deck = [];
-let dScore = 0;
+
+// Custom event listener : when a new card is added to dealer's hand, sum up the score
+$(".dealercards").on("cardAdded", function () {
+  let score = 0;
+
+  $(".dealercards img").each(function () {
+    let src = $(this).attr("src");
+    let filename = src.split("/").pop();
+    let number = filename.split("_")[0];
+    score += parseInt(number);
+  });
+
+  $(".dealerscore").text(score);
+});
+
+// Custom event listener : when a new card is added to player's hand, sum up the score
+$(".playercards").on("cardAdded", function () {
+  let score = 0;
+
+  $(".playercards img").each(function () {
+    let src = $(this).attr("src");
+    let filename = src.split("/").pop();
+    let number = filename.split("_")[0];
+    score += parseInt(number);
+  });
+
+  $(".playerscore").text(score);
+});
 
 // Deck generation, Ref: https://mebee.info/2022/08/24/post-71799/
 for (let i = 0; i < suit.length; i++) {
@@ -20,6 +47,8 @@ function opening(deck) {
     const card = drawCard(deck);
     displayCard("playercards", card);
   }
+  //Score calculation
+  $(".playercards").trigger("cardAdded");
 
   const dealerCard = drawCard(deck);
   $(".dealercards").append(
@@ -28,6 +57,8 @@ function opening(deck) {
   $(".dealercards").prepend(
     `<img src="./image/cards/0_back.png" alt="cards" width="130" height="189" class="back">`
   );
+  //Score calculation
+  $(".dealercards").trigger("cardAdded");
 }
 
 // Drawing a card
@@ -49,6 +80,9 @@ function displayCard(targetClass, card) {
       `<img src="./image/cards/${card.num}_${card.suit}.png" alt="cards" width="130" height="189">`
     );
   }
+
+  //Score calculation
+  $(`.${targetClass}`).trigger("cardAdded");
 }
 
 // Hit button
@@ -63,13 +97,3 @@ $("#stand-button").click(function () {
   const card = drawCard(deck);
   displayCard("dealercards", card);
 });
-
-// Sum up the score
-$(".dealercards img").each(function () {
-  let src = $(this).attr("src");
-  let filename = src.split("/").pop();
-  let number = filename.split("_")[0];
-  dScore += parseInt(number);
-});
-
-$(".dealerscore").text(isNaN(dScore) ? 0 : dScore);
